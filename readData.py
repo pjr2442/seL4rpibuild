5751f13ee309a5fd4b0940a18463bcec88d8e37c
pjr2442 <pjr2442@users.noreply.github.com>
2017-07-03 15:08:28 -0400

    Add files via upload

---
+++ readData.py
@@ -0,0 +1,8 @@
+import Putty
+
+ser = serial.Serial('/dev/ttyUSB0', 2000000, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False) #Tried with and without the last 3 parameters, and also at 1Mbps, same happens.
+ser.flushInput()
+ser.flushOutput()
+While True:
+  data_raw = ser.readline()
+  print data_raw
